253852b3fad0f9ab569f5fa02a50243ac3c50077
Krzysiek <krzysztof.orawski@wp.pl>
2026-01-29 12:35:29 +0100

    add fix after code review

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -2,9 +2,6 @@ package core.basesyntax;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 public class SalaryInfo {
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
@@ -12,30 +9,36 @@ public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         LocalDate from = LocalDate.parse(dateFrom, formatter);
         LocalDate to = LocalDate.parse(dateTo, formatter);
-        Map<String, Integer> salaryMap = new HashMap<>();
+        StringBuilder report = new StringBuilder("Report for period "
+                + dateFrom
+                + " - "
+                + dateTo
+                + System.lineSeparator());
 
-        for (String line : data) {
-            String[] parts = line.split(" ");
-            LocalDate currentDate = LocalDate.parse(parts[0], formatter);
-            String name = parts[1];
+        for (String name : names) {
+            int totalSalary = 0;
+            for (String line : data) {
+                String[] parts = line.trim().split("\\s+");
+                LocalDate currentDate = LocalDate.parse(parts[0], formatter);
+                String currentName = parts[1];
 
-            if (!Arrays.asList(names).contains(name)) {
-                continue;
+                if (!name.equalsIgnoreCase(currentName)) {
+                    continue;
+                }
+
+                if (currentDate.isBefore(from) || currentDate.isAfter(to)) {
+                    continue;
+                }
+                int hours = Integer.parseInt(parts[2]);
+                int rate = Integer.parseInt(parts[3]);
+                totalSalary += hours * rate;
             }
+            report.append(name)
+                    .append(" - ")
+                    .append(totalSalary)
+                    .append(System.lineSeparator());
+        }
 
-            if (currentDate.isBefore(from) || currentDate.isAfter(to)) {
-                continue;
-            }
-            int hours = Integer.parseInt(parts[2]);
-            int rate = Integer.parseInt(parts[3]);
-            salaryMap.put(name, salaryMap.getOrDefault(name, 0) + hours * rate);
-            }
-
-            StringBuilder report = new StringBuilder("Report for period " + dateFrom + " - " + dateTo + System.lineSeparator());
-            for (String name : names) {
-                report.append(name).append(" - ").append(salaryMap.getOrDefault(name, 0)).append(System.lineSeparator());
-            }
-
-            return report.toString().trim();
+        return report.toString().trim();
     }
 }
